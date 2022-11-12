3ecaba63ea77ce3faa58eb9a70fe2bb2359e8e90
Savannah Mercedes <savannahmunn1996@gmail.com>
2022-11-11 18:26:06 -0600

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const startMin = 8;
+let time = startMin * 60;
+const countDownEl = document.getElementById("timer");
+setInterval(letsCount, 1000);
+
+function letsCount {
+    const minutes = math.floor(time / 60);
+    let seconds = time % 60;
+    countDownEl.innerHTML = ${'minutes'} ${ 'seconds' };
+    time--;
+}
