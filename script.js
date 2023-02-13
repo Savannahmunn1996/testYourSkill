468ec04dbbaa23385f29c86a47d39bcfcfd2a301
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-12 19:46:51 -0800

    completed quiz functionality

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const timeGauge = document.getElementById("timeGauge");
 let thisTime = document.getElementById("timer");
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const Qcontainer = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
-let shuffledQ;
-let QIndex;
+let shuffledQ, QIndex;
 
 startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+  QIndex++;
+  nextQ();
+});
 
 function startQuiz() {
   console.log("we started!");
@@ -19,14 +23,60 @@ function startQuiz() {
 }
 
 function nextQ() {
+  resetState();
   showQuestion(shuffledQ[QIndex]);
 }
 
 function showQuestion(question) {
-  questionElement.innerText = questions.question;
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButton.appendChild(button);
+  });
 }
 
-function selectAnswer() {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButton.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQ.length > QIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 let questions = [
   {
@@ -65,56 +115,83 @@ let questions = [
       },
     ],
   },
-  //   {
-  //     question: "Is javascript a case sensitive language?",
+  {
+    question: "Is javascript a case sensitive language?",
+    answers: [
+      { text: "No", correct: false },
 
-  //     choiceA: "No",
+      { text: "I dont know", correct: false },
 
-  //     choiceB: "I dont know",
+      { text: "yes", correct: true },
+    ],
+  },
+  {
+    question: "A boolean is an HTML data type.",
+    answers: [
+      { text: "Yes of course!", correct: false },
 
-  //     choiceC: "yes",
+      { text: "No, It is a javascript data type.", correct: true },
 
-  //     correct: "C",
-  //   },
-  //   {
-  //     question: "A boolean is an HTML data type.",
+      { text: "It could be, who knows.", correct: false },
+    ],
+  },
+  {
+    question: "What are CSS selectors?",
+    answers: [
+      {
+        text: "Css selectors are used to set up projects on vs code.",
+        correct: false,
+      },
 
-  //     choiceA: "Yes of course!",
+      {
+        text: " Css selectors are used to select HTML elements based on their element name, id, attributes, etc.",
+        correct: true,
+      },
 
-  //     choiceB: "No, It is a javascript data type.",
+      {
+        text: " CSS selectors are used to select javascript funtions based on their event listeners.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "How do you create a function in javascript?",
+    answers: [
+      { text: "function savannahMunn()", correct: true },
 
-  //     choiceC: "It could be, who knows.",
+      { text: " functionis savannahMunn", correct: false },
 
-  //     correct: "B",
-  //   },
-  //   {
-  //     question: "What are CSS selectors?",
-
-  //     choiceA: "Css selectors are used to set up projects on vs code.",
-
-  //     choiceB:
-  //       " Css selectors are used to select HTML elements based on their element name, id, attributes, etc.",
-
-  //     choiceC:
-  //       " CSS selectors are used to select javascript funtions based on their event listeners.",
-
-  //     correct: "B",
-  //   },
-  //   {
-  //     question: "How do you create a function in javascript?",
-
-  //     choiceA: "function savannahMunn()",
-
-  //     choiceB: " functionis savannahMunn",
-
-  //     choiceC: "...functionsav",
-
-  //     correct: "A",
-  //   },
+      { text: "...functionsav", correct: false },
+    ],
+  },
+  {
+    question: "What is the purpose of the This operator in JavaScript?",
+    answers: [
+      { text: "It can be used to create color themes.", correct: false },
+      { text: "It has no purpose.", correct: false },
+      {
+        text: "The JavaScript this keyword refers to the object it belongs to.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: " What is NaN in JavaScript?",
+    answers: [
+      {
+        text: "Since 2 and 5 are integers, they will be added numerically.",
+        correct: false,
+      },
+      { text: "NaN is a short form of Not a Number.", correct: true },
+      {
+        text: " It takes the string to be converted as its first parameter",
+        correct: false,
+      },
+    ],
+  },
 ];
 
-// const lastQuestion = questions.length - 1;
-// let firstQuestion = 0;
+
 
 // let timer;
 // // set up a timer 8 minutes
