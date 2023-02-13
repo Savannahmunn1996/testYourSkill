eb6b677673e54c2e8c536c2e75b715eddecd7c5d
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-12 20:31:18 -0800

    timer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const timeGauge = document.getElementById("timeGauge");
-let thisTime = document.getElementById("timer");
+
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const Qcontainer = document.getElementById("question-container");
@@ -13,7 +13,37 @@ nextButton.addEventListener("click", () => {
   nextQ();
 });
 
+document.getElementById("timer").innerHTML = 05 + ":" + 00;
+
+function startTimer() {
+  var presentTime = document.getElementById("timer").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(timeArray[1] - 1);
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 0) {
+    return;
+  }
+
+  document.getElementById("timer").innerHTML = m + ":" + s;
+  console.log(m);
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  } // add zero in front of numbers < 10
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
+
 function startQuiz() {
+  startTimer();
   console.log("we started!");
   startButton.classList.add("hide");
   shuffledQ = questions.sort(() => Math.random() - 0.5);
@@ -190,8 +220,6 @@ let questions = [
     ],
   },
 ];
-
-
 
 // let timer;
 // // set up a timer 8 minutes
